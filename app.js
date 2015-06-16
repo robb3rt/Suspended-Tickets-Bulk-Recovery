287adca23818880fefa3484469c8ad0d301e6144
z3nRob <rhink@zendesk.com>
2015-06-16 17:27:45 +0100

    added comments and todo items
    added comments and todo items

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function() {
 
   return {
-      
+      defaultState: 'loading',
       requests: {
             getSuspendedTickets: function(query){
                 return {
@@ -17,28 +17,32 @@
                     dataType: "json"
                 };
             },
-      }
+      },
     events: {
       'app.activated':'doSomething'
     },
 
     doSomething: function() {
         //get suspended tickets.
-        this.switchTo('loading');
-        this.ajax('getSearchResults', query)
+        console.dir("test");
+        this.ajax('getSuspendedTickets')
             .done(function(data) {
-                if (data.count === 0){this.switchTo('searchbar');return;}
+                if (data.count === 0){this.switchTo('suspendtypes');return;} //TODO: Add layout for nothing found
 				var size = Math.ceil(data.count/100);
 				var results = (size == 1) ? new Array(0) : new Array(size-2);
 				results.push(data.results);
 				for (var i = 1; i < size; i++){
 				    /*jshint loopfunc: true */
-				    this.ajax('getSearchResultsByPage', query, i+1)
+				    this.ajax('getSuspendedTicketsByPage', i+1)
 				        .done(function(restdata){
 				            results.push(restdata.results);
 				        });
 				}
             });
+        //scan through results and find the different causes of suspension. Update the dropdow fields
+        //allow user to select causes of suspension.
+        //Add submit button to recover these tickets.
+        console.dir(results);
         this.switchTo('suspendtypes');
     }
   };
