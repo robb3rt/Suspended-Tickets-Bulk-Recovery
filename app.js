8ae97775486727a1fd156f23cd5b49a367fc3ae6
z3nRob <rhink@zendesk.com>
2015-06-17 17:31:45 +0100

    added template changes
    template changes

--- app.js
+++ app.js
@@ -24,26 +24,33 @@
 
     doSomething: function() {
         //get suspended tickets.
-        console.dir("test");
         this.ajax('getSuspendedTickets')
             .done(function(data) {
                 if (data.count === 0){this.switchTo('suspendtypes');return;} //TODO: Add layout for nothing found
 				var size = Math.ceil(data.count/100);
-				var results = (size == 1) ? new Array(0) : new Array(size-2);
-				results.push(data.results);
+				var results = new Array(size-1);
+				results.push(data.suspended_tickets);
+                var causes = [];
 				for (var i = 1; i < size; i++){
 				    /*jshint loopfunc: true */
 				    this.ajax('getSuspendedTicketsByPage', i+1)
 				        .done(function(restdata){
-				            results.push(restdata.results);
+				            results.push(restdata.suspended_tickets);
 				        });
 				}
+                //console.dir(results);
+                for (var a = 0; a < results.length; a++){
+                    for (var b = 0; b < results[a].length; b++){
+                        causes.push(results[a][b].cause);
+                    }
+                }
+                causes = _.uniq(causes);
+                console.dir(causes);
+                this.switchTo('suspendtypes', {
+							causes: causes
+				});
+                //TODO: add functionality to recover tickets for the cause of suspension
             });
-        //scan through results and find the different causes of suspension. Update the dropdow fields
-        //allow user to select causes of suspension.
-        //Add submit button to recover these tickets.
-        console.dir(results);
-        this.switchTo('suspendtypes');
     }
   };
 
